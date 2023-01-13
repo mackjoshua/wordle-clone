c4cf8b284b55d92a625ba2165a46a8a9234d19f1
Joshua <joshmack02@gmail.com>
2023-01-13 14:37:43 -0800

    User can print to the DOM on all lines

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-let letterArray = [];
+let guess1 = [];
+let guess2 = [];
+let guess3 = [];
+let guess4 = [];
+let guess5 = [];
+let guess6 = [];
+
+const guesses = [guess1,guess2,guess3,guess4,guess5,guess6];
+
 
 const row1 = document.querySelector('#row-1');
 const row2 = document.querySelector('#row-2');
@@ -13,40 +21,85 @@ const space3 = space2.nextElementSibling;
 const space4 = space3.nextElementSibling;
 const space5 = space4.nextElementSibling;
 
+const space6 = row2.firstElementChild;
+const space7 = space6.nextElementSibling;
+const space8 = space7.nextElementSibling;
+const space9 = space8.nextElementSibling;
+const space10 = space9.nextElementSibling;
 
-let theGuess = [];
-let spaces = [space1,space2,space3,space4,space5];
-let tempKey = ['s','t','o','n','e'];
+const space11 = row3.firstElementChild;
+const space12 = space11.nextElementSibling;
+const space13 = space12.nextElementSibling;
+const space14 = space13.nextElementSibling;
+const space15 = space14.nextElementSibling;
 
+const space16 = row4.firstElementChild;
+const space17 = space16.nextElementSibling;
+const space18 = space17.nextElementSibling;
+const space19 = space18.nextElementSibling;
+const space20 = space19.nextElementSibling;
+
+const space21 = row5.firstElementChild;
+const space22 = space21.nextElementSibling;
+const space23 = space22.nextElementSibling;
+const space24 = space23.nextElementSibling;
+const space25 = space24.nextElementSibling;
+
+const space26 = row6.firstElementChild;
+const space27 = space26.nextElementSibling;
+const space28 = space27.nextElementSibling;
+const space29 = space28.nextElementSibling;
+const space30 = space29.nextElementSibling;
+
+let spaces1 = [space1,space2,space3,space4,space5];
+let spaces2 = [space6,space7,space8,space9,space10];
+let spaces3 = [space11,space12,space13,space14,space15];
+let spaces4 = [space16,space17,space18,space19,space20];
+let spaces5 = [space21,space22,space23,space24,space25];
+let spaces6 = [space26,space27,space28,space29,space30];
+
+const rowsArray = [spaces1,spaces2,spaces3,spaces4,spaces5,spaces6];
+
+let wordOfTheDay;
+let rowsIndex = 0;
+let guessIndex = 0;
 
 // This function runs a for loop that prints the value of our array, which previously had a letter pushed to it,
 // to the DOM by setting the innerText value of the element of spaces at the same index. The values are all <td> elements
 
-function firstGame() {
-    for(let i = 0 ; i < spaces.length; i++) {
-        spaces[i].innerText = letterArray[i] || '';
+// function firstGame() {
+//     for(let i = 0 ; i < spaces1.length; i++) {
+//         spaces1[i].innerText = letterArray[i] || '';
+//     }
+// }
+
+function runGameTest() {
+    for(let i = 0 ; i < rowsArray[rowsIndex].length; i++) {
+        rowsArray[rowsIndex][i].innerText = guesses[guessIndex][i] || '';
     }
 }
 
 
+function nextItem() {
+    rowsArray[rowsIndex].runGameTest();
+    rowsIndex++;
+}
+
+document.addEventListener('keyup', () => {});
+
 // This function compares the user input against the key
 function evaluateWord (key, userWord) {
     for (let i = 0; i < 5 ; i++) {
-        spaces[i].style.backgroundColor = 'white';
+        spaces1[i].style.backgroundColor = 'white';
 
         if (key.at(i) == userWord.at(i)) {
-            spaces[i].style.backgroundColor = 'green';
+            spaces1[i].style.backgroundColor = 'green';
             console.log(`The index at ${i} matches`);
         } else if (key.includes(userWord.at(i)) && (key.at(i) != userWord.at(i))){
                 let index = userWord.includes(key[i]);
                 console.log(userWord.indexOf(index) + ' here I am');
-                spaces[i].style.backgroundColor = 'yellow';
+                spaces1[i].style.backgroundColor = 'yellow';
         }
-
-        
-
-       
-
     }
 }
 
@@ -55,12 +108,20 @@ function moveRow () {
 }
 
 
-// This function joins our letter array to a string so that it can be evaluated against the word of the day
-function grabWord () {
-    const word = letterArray.join('');
-    console.log(word);
-    return word;
+// This function connects to an API to get the word of the day
+
+async function getWordToday () {
+    const promise = await fetch('https://words.dev-apis.com/word-of-the-day');
+    const processedResponse = await promise.json();
+    console.log(typeof processedResponse);
+    return processedResponse;
 }
+
+// This function sets the word of the day
+getWordToday().then(function (object) {
+    wordOfTheDay = object.word;
+    console.log(object.word);
+});
 
 // This event listener takes user input, verifies it's a letter, and then pushes it to our array
 // Also, it runs the first game function as well, so every time the event fires, the firstGame() fires too
@@ -68,39 +129,43 @@ function grabWord () {
 
 document.addEventListener("keyup", function(event) {
     if (event.key.match(/^[a-zA-Z]$/)) {
-          letterArray.push(event.key);
-          firstGame();
-          console.log(letterArray);
+          guesses[guessIndex].push(event.key);
+        //   firstGame();
+          runGameTest();
+        //   console.log(letterArray);
       }
   });
 
 
 // This event listener runs the grabWord function every time the enter button is pressed
+// and the evaluate word function
 document.addEventListener('keyup', (e) => {
     if (e.key == 'Enter') {
-        if (letterArray.length !== 5) {
+        if (guesses[guessIndex].length !== 5) {
             return
         } else {
-            grabWord();
-            evaluateWord(tempKey, letterArray);
-            moveRow();
-            console.log(letterArray);
+            evaluateWord(wordOfTheDay, guesses[guessIndex]);
+            // moveRow();
+            // console.log(letterArray);
+            // letterArray = [];
+            runGameTest();
+            rowsIndex++;
+            guessIndex++;
         }
     }
 });
 
 // This event listener pops the last item off of the letterArray, and runs the firstGame() function to 
 // repopulate the DOM
-let currentIndex = 0;
 document.addEventListener("keyup", function(event) {
     if (event.key === "Backspace") {
-        // letterArray.splice(currentIndex - 1, 1);
-        letterArray.pop()
-        // currentIndex--;
-        firstGame();
-        console.log(letterArray);
+        guesses[guessIndex].pop()
+        runGameTest();
+        // console.log(letterArray);
     }
 });
 
-console.log(letterArray);
+
+// console.log(letterArray);
+
 
